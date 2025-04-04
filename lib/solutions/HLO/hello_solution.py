7a8ae240af28ce0aa674e730c138af41a5598c2e
sbx_user1051 <sbx_user1051@169.254.24.89>
2025-04-04 17:57:25 +0000

    Fri Apr 04 17:57:25 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,12 +1,14 @@
 
 class HelloSolution:
-    
-    # friend_name = unicode string
     def hello(self, friend_name):
-        if isinstance(friend_name, str):
-            return f"Hello, {friend_name}!"
-        else:
-            raise ValueError("friend_name must be a string")
+        print(f"[DEBUG] Responding to: {friend_name}")
+        if not isinstance(friend_name, str):
+            raise TypeError("friend_name must be a string")
+        if not friend_name:
+            raise ValueError("friend_name is required")
+        return f"Hello, {friend_name}!"
+
+
 
 
 
