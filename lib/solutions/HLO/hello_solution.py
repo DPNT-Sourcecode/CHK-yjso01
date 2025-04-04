33813365ed0c654d98f32cfdcdc598a799d58caa
sbx_user1051 <sbx_user1051@169.254.24.89>
2025-04-04 17:41:13 +0000

    Fri Apr 04 17:41:13 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -6,7 +6,7 @@ class HelloSolution:
         if not friend_name:
             raise ValueError("friend_name is required")
         else:
-            return f"Hello World!"
+            return f"Hello World"
 
         # raise NotImplementedError()
     # IS THERE MEANT TO BE A TEST FILE FOR THIS CHALLENGE???????
@@ -29,8 +29,3 @@ class HelloSolution:
     # IS THERE MEANT TO BE A TEST FILE FOR THIS CHALLENGE???????
     # IS THERE MEANT TO BE A TEST FILE FOR THIS CHALLENGE???????
     # IS THERE MEANT TO BE A TEST FILE FOR THIS CHALLENGE???????
-
-
-
-
-
