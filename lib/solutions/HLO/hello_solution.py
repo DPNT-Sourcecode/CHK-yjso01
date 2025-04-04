f265822aa39cc3215ad6b52e9d363539a2744cfc
sbx_user1051 <sbx_user1051@169.254.24.89>
2025-04-04 17:27:47 +0000

    Fri Apr 04 17:27:47 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,10 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-        print(f"Hello {friend_name}!")
+        if not friend_name:
+            raise ValueError("friend_name cannot be empty")
+        else:
+            return f"Hello {friend_name}!"
+
         # raise NotImplementedError()
+
