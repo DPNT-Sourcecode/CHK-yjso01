7df4cbb48438335c1419f877fc3f3e8dd33dd364
sbx_user1051 <sbx_user1051@169.254.4.25>
2025-04-05 22:02:35 +0000

    Sat Apr 05 22:02:35 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,8 +3,8 @@ class HelloSolution:
     
     # friend_name = unicode string
     def hello(self, friend_name):
-        if friend_name:
-            return "Hello, World!"
+        return "Hello, World!"
+
 
 
 
