d7ea7a4bb179cc067e3b1034397f24957d671516
sbx_user1051 <sbx_user1051@169.254.64.245>
2025-04-06 22:57:24 +0000

    Sun Apr 06 22:57:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,8 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+    # calculate the total checkout price
+    # item A: 50, B: 30, C: 20, D: 15
+    # special offers: 3A 130, 2B for 45
+    # illegal input returns -1
+
