97d70de621f7c0dc533077f51ddd42d010669c10
sbx_user1051 <sbx_user1051@169.254.64.245>
2025-04-06 23:00:24 +0000

    Sun Apr 06 23:00:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,3 +8,26 @@ class CheckoutSolution:
     # special offers: 3A 130, 2B for 45
     # illegal input returns -1
 
+        count_A = 0
+        count_B = 0
+        count_C = 0
+        count_D = 0
+        total_price = 0
+
+        # count the number of each item
+        for item in skus:
+            if item == "A":
+                count_A += 1
+            elif item == "B":
+                count_B += 1
+            elif item == "C":
+                count_C += 1
+            elif item == "D":
+                count_D += 1
+            else:
+                return -1
+        
+        # calculate the total price
+
+
+
