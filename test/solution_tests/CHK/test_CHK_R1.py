7fe44d4b87c0d0ef957a1c19bd7d5647e39b44b4
sbx_user1051 <sbx_user1051@169.254.64.245>
2025-04-06 23:35:29 +0000

    Sun Apr 06 23:35:29 UTC 2025

--- test/solution_tests/CHK/test_CHK_R1.py
+++ test/solution_tests/CHK/test_CHK_R1.py
@@ -11,16 +11,16 @@ def test_special_offers():
     assert CheckoutSolution().checkout("AAA") == 130
     assert CheckoutSolution().checkout("BB") == 45
     assert CheckoutSolution().checkout("EEB") == 40
-    assert CheckoutSolution().checkout("EEBB") == 120
-    assert CheckoutSolution().checkout("EEEBBBB") == 195
 
 def test_combined_items():
     assert CheckoutSolution().checkout("ABCD") == 115
     assert CheckoutSolution().checkout("AABBC") == 165
-    assert CheckoutSolution().checkout("AAABBB") == 205 
+    assert CheckoutSolution().checkout("AAABBB") == 205
+    assert CheckoutSolution().checkout("EEBB") == 110
+    assert CheckoutSolution().checkout("EEEBBBB") == 195
 
 def test_invalid_input():
-    assert CheckoutSolution().checkout("E") == -1
+    assert CheckoutSolution().checkout("F") == -1
     assert CheckoutSolution().checkout("AABCF") == -1
     assert CheckoutSolution().checkout("123") == -1
     assert CheckoutSolution().checkout("") == 0
