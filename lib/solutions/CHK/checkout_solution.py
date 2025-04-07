3e9f97801c0cf9497c66725f5c3c851a3424e631
sbx_user1051 <sbx_user1051@169.254.64.245>
2025-04-07 00:26:46 +0000

    Mon Apr 07 00:26:46 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -85,6 +85,18 @@ class CheckoutSolution:
             special_offer_2K = 0
             price_K = count_K * 80
 
+        # 3N get one M free
+        count_N = count[ord("N") - 65]
+        count_M = count[ord("M") - 65]
+        if count_N >= 3:
+            special_offer_3N = count_N // 3 * 40
+            price_M = count_M - count_N // 3
+            if price_M < 0:
+                price_M = 0
+        else:
+            special_offer_3N = 0
+            price_M = count_M
+
         total_price = special_offer_5A + special_offer_3A + price_A + special_offer_B + price_B + price_C + price_D + price_E + special_offer_F + price_F
 
         return total_price
