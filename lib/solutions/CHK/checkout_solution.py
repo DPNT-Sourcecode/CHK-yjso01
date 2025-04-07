84602cd04d92c1d6195da6383ef10aabdaabf7a1
sbx_user1051 <sbx_user1051@169.254.64.245>
2025-04-07 00:17:46 +0000

    Mon Apr 07 00:17:46 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,21 +3,6 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-    # calculate the total checkout price
-    # item A: 50, B: 30, C: 20, D: 15, E: 40, F: 10
-    # special offers:
-        # 5A 200, 3A 130,
-        # 2B for 45
-        # 2E get 1B free
-        # 2F get 1F free
-    # illegal input returns -1
-
-        # count_A = 0
-        # count_B = 0
-        # count_C = 0
-        # count_D = 0
-        # count_E = 0
-        # count_F = 0
         prices = [50, 30, 20, 15, 40, 10, 20, 10, 35, 60, 80, 90, 15, 40, 10, 50, 30, 50, 30, 20, 40, 50, 20, 90, 10, 15]
         count = [0]*26
         total_price = 0
@@ -28,14 +13,22 @@ class CheckoutSolution:
                 return -1
             count[ord(item) - 65] += 1
         
-        # calculate the total price
-        
-        # how many free B's from E offer
+        # calculate the total price through the special offers----Main part-----
+        # 2B for 45 and 2E get 1B free offer
         count_E = count[ord("E") - 65]
+        count_B = count[ord("B") - 65]
+
         free_B = count_E // 2
 
-        # check for 5A's, 3A's and A's
+        if free_B > count_B:
+            price_B, special_offer_B = 0, 0
+        else:
+            special_offer_B = (count_B - free_B) // 2 * 45
+            price_B = (count_B - free_B) % 2 * 30
+
+        # 5A's for 200 and 3A's for 130 offer
         count_A = count[ord("A") - 65]
+
         if count_A >= 5:
             special_offer_5A = count_A // 5 * 200
             if count_A % 5 >= 3:
@@ -53,18 +46,8 @@ class CheckoutSolution:
                 special_offer_5A, special_offer_3A = 0, 0
                 price_A = count_A * 50
 
-        # check if there are more free B's than B's in the cart
-        if free_B > count_B:
-            price_B, special_offer_B = 0, 0
-        else:
-            special_offer_B = (count_B - free_B) // 2 * 45
-            price_B = (count_B - free_B) % 2 * 30
-
-        price_C = count_C * 20
-
-        price_D = count_D * 15
-
-        price_E = count_E * 40
+        # 2F get one F free offer
+        count_F = count[ord("F") - 65]
 
         if count_F >= 3:
             special_offer_F = 2 * (count_F // 3) * 10
@@ -76,5 +59,6 @@ class CheckoutSolution:
         total_price = special_offer_5A + special_offer_3A + price_A + special_offer_B + price_B + price_C + price_D + price_E + special_offer_F + price_F
 
         return total_price
+
 
 
